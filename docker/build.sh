693bcccdff4ae3ddc1181e406536e6e59aa671a8
pbillerot <philippe.billerot@gmail.com>
2023-07-24 19:07:12 +0200

    git reset

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,6 @@
 
 
-# docker ps -a | grep beethon | awk '{print $1}' | xargs docker rm -f
+docker ps -a | grep beethon | awk '{print $1}' | xargs docker rm -f
 # docker build --no-cache -t beethon .
-docker-compose up -d --build 
+docker-compose up -d --build
 docker image prune -f
